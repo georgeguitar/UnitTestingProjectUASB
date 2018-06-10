88edbe6633d38ce11d906c381ea642908a1a8ebb
Juan Navarro <jdna0615@gmail.com>
2018-06-10 01:04:12 -0400

    pruebas añadidas y arreglos

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -14,45 +14,25 @@ public class Analyzer {
 	 */
 	public static List<Sentence> readFile(String filename) {
 		List<Sentence> listSentences = new ArrayList<Sentence>();
-		boolean error = false;
-		
-		// Fichero del que queremos leer
 		File fichero = new File(filename);
 		Scanner s = null;
 
 		try {
-			// Leemos el contenido del fichero
-			System.out.println("... Leemos el contenido del fichero ...");
 			s = new Scanner(fichero);
 
-			// Leemos linea a linea el fichero
 			while (s.hasNextLine()) {
 				String linea = s.nextLine(); 	// Guardamos la linea en un String
 				System.out.println(linea);      // Imprimimos la linea
-				
+
 				String score = linea.substring(0, linea.indexOf(" "));
-				
-				/**
-				 * http://txt2re.com/index-java.php3?s=-2%20I%20would%20have%20preferred%20an%20easier%20assignment&11&17&7
-				 * https://regexr.com/
-				 * 
-				 *     String re1="([-+]\\d+)";	// Integer Number 1
-					    String re2="(\\s+)";	// White Space 1
-					    String re3=".*?";	// Non-greedy match on filler
-					    String re4="((?:[a-z][a-z]+))";	// Word 1
-				 * 
-				 * **/
-				
-//				if (score.matches("-?\\d+(\\.\\d+)?")) {
-				
+
 				Integer puntaje;
-//				if (score.matches("[-+]\\d+")) {
-				if (score.matches("^[+-]?[0-2]{1}$")) {				
+				if (score.matches("^[+-]?[0-2]{1}$")) {
 					puntaje = Integer.valueOf(score);
 					if (puntaje >= -2 && puntaje <= 2) {
 						String text = linea.substring(linea.indexOf(" ") + 1, linea.length());
 						Sentence sentence = new Sentence(puntaje, text);
-						listSentences.add(sentence);						
+						listSentences.add(sentence);
 					}
 				}
 			}
@@ -67,7 +47,7 @@ public class Analyzer {
 				System.out.println("Mensaje 2: " + ex2.getMessage());
 			}
 		}
-		
+
 		return listSentences;
 
 	}
@@ -76,27 +56,23 @@ public class Analyzer {
 	 * Implement this method in Part 2
 	 */
 	public static Set<Word> allWords(List<Sentence> sentences) {
-    	Set<Word> listaPalabras = new TreeSet<Word>();
-    	for (Sentence sentence : sentences) {
-    		String[] palabras = sentence.text.split(" ");
-    		for (String palabra : palabras) {
-    			Word word = new Word(palabra.toLowerCase().trim());
-    			listaPalabras.add(word);
-    		}
-    	}
-    	for (Word palabra : listaPalabras) {
-    		String palabraBuscar = palabra.getText();
-        	for (Sentence sentence : sentences) {
-        		String[] palabras = sentence.text.split(" ");
-        		for (String palabr : palabras) {
-        			if (palabraBuscar.equals(palabr.toLowerCase().trim())) {
-        				palabra.increaseTotal(sentence.getScore());
-        			}
-        		}
-        		
-        	}
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		for (Sentence sentence : sentences) {
+			String[] palabras = sentence.text.split(" ");
+			for (String palabra : palabras) {
+				Word word = new Word(palabra.toLowerCase().trim());
+				listaPalabras.add(word);
+			}
 		}
-    	
+		for (Word palabra : listaPalabras) {
+			String palabraBuscar = palabra.getText();
+			for (Sentence sentence : sentences) {
+				if (sentence.getText().toLowerCase().trim().contains(palabraBuscar.toLowerCase().trim())) {
+					palabra.increaseTotal(sentence.getScore());
+				}
+			}
+		}
+
 		return listaPalabras;
 	}
 
--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -14,38 +14,119 @@ import java.util.TreeSet;
 import org.junit.Test;
 
 public class AnalyzerTest {
-    @Test
-    public void testSomeLibraryMethod() {
-    	//Sentence sentence = new Sentence();
-    	List<Sentence> lista = new ArrayList<Sentence>(); 
-    	lista = Analyzer.readFile("archivo.txt");
-    	
-        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
-    
-    @Test
-    public void testSomeLibraryMethod1() {
-    	Set<Word> listaPalabras = new TreeSet<Word>();
-    	List<Sentence> lista = new ArrayList<Sentence>(); 
-    	lista = Analyzer.readFile("archivo.txt");
-    	listaPalabras = Analyzer.allWords(lista);
-    	
-        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
-    
-    @Test
-    public void testSomeLibraryMethod2() {
-    	Set<Word> listaPalabras = new TreeSet<Word>();
-    	List<Sentence> lista = new ArrayList<Sentence>(); 
-    	lista = Analyzer.readFile("archivo.txt");
-    	listaPalabras = Analyzer.allWords(lista);
-    	
-    	Map<String, Double> mapa = new HashMap<String, Double>();
-    	mapa = Analyzer.calculateScores(listaPalabras);
-    	
-    	for (Map.Entry<String, Double> entry : mapa.entrySet()) {
-    		System.out.println(entry.getKey() + ": " + entry.getValue());
-    	}
-        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+	//readFile
+	@Test
+	public void testNoExisteArchivo() {
+		//cuando el arcchivo no se pueda abrir
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("no_existe_arcchivo.txt");
+		assertTrue(lista.isEmpty());
+	}
+
+	@Test
+	public void testReadFileEmpty() {
+		List<Sentence> ListaVaciaResult=Analyzer.readFile("archivo_vacio.txt");
+		assertEquals("", 0, ListaVaciaResult.size());
+	}
+
+	@Test
+	public void testFormatoIncorrecto() {
+		List<Sentence> lista = Analyzer.readFile("archivo_formato_incorrecto.txt");
+		assertEquals(3, lista.size());
+	}
+
+	@Test
+	public void testFormatoIncorrectoLimites() {
+		List<Sentence> lista = Analyzer.readFile("archivo_formato_limites.txt");
+		assertEquals(2, lista.size());
+	}
+
+
+
+	/*
+	 * Implement this method in Part 2
+	 */
+	//allWords
+	//el numero de palabras sea correcto
+	@Test
+	public void testNumeroPalabras() {
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("archivo.txt");
+		listaPalabras = Analyzer.allWords(lista);
+		assertEquals(40, listaPalabras.size());
+	}
+
+
+	//cantidad de apariciones.
+	@Test
+	public void testDatosCorrectos() {
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("archivo.txt");
+		listaPalabras = Analyzer.allWords(lista);
+
+		int count = 0;
+		for (Word palabra : listaPalabras) {
+			if("java".equals(palabra.getText())){
+				count = palabra.getCount();
+			}
+		}
+
+		assertEquals(2, count);
+	}
+
+	//acumulativo
+	@Test
+	public void testDatosCorrectosAcumulativo() {
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("archivo.txt");
+		listaPalabras = Analyzer.allWords(lista);
+
+		int total = 0;
+		for (Word palabra : listaPalabras) {
+			if("java".equals(palabra.getText())){
+				total = palabra.getTotal();
+			}
+		}
+		assertEquals(4, total);
+	}
+
+
+	/*
+	 * Implement this method in Part 3
+	 */
+	@Test
+	public void testScore() {
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("archivo.txt");
+		listaPalabras = Analyzer.allWords(lista);
+
+		Map<String, Double> mapa = new HashMap<String, Double>();
+		mapa = Analyzer.calculateScores(listaPalabras);
+
+		double score = 0;
+		for (Map.Entry<String, Double> entry : mapa.entrySet()) {
+			if ("fun".equals(entry.getKey())) {
+				score = entry.getValue();
+			}
+		}
+		assertEquals("0.8", String.valueOf(score));
+	}
+
+	@Test
+	public void testMapaVacio() {
+		Set<Word> listaPalabras = new TreeSet<Word>();
+		List<Sentence> lista = new ArrayList<Sentence>();
+		lista = Analyzer.readFile("archivo_vacio.txt");
+		listaPalabras = Analyzer.allWords(lista);
+
+		Map<String, Double> mapa = new HashMap<String, Double>();
+		mapa = Analyzer.calculateScores(listaPalabras);
+
+
+		assertEquals(0, mapa.size());
+	}
 }
